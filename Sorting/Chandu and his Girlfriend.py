2f26ae12f941303a5b873505e71b35529588911c
Shivam sharma <shivams334@gmail.com>
2016-09-13 20:16:12 +0530

    Update Chandu and his Girlfriend.py

--- Sorting/Chandu and his Girlfriend.py
+++ Sorting/Chandu and his Girlfriend.py
@@ -1,16 +1,7 @@
-'''
-# Read input from stdin and provide input before running code
-
-name = raw_input('What is your name?\n')
-print 'Hi, %s.' % name
-'''
-x=input();
-while x > 0:
-	x=x-1;
-	n=input();
-	a=map(int,raw_input().split());
-	a.sort();
-	while n > 0 :
-		print a[n-1],
-		n-=1;
-	print "";
+    tc = input()
+    for _ in range(tc):
+        n = input()
+        arr = map(eval, raw_input().split())
+        arr.sort(reverse=True)
+        some =  ' '.join(str(x) for x in arr)
+        print some
