119d4714bafec09bb3490738aaa96bfb8384b5a5
Shivam sharma <shivams334@gmail.com>
2016-09-15 20:39:23 +0530

    Update Monk's Choice of Numbers.py

--- Bit Manipulation/Monk's Choice of Numbers.py
+++ Bit Manipulation/Monk's Choice of Numbers.py
@@ -1,13 +1,17 @@
-t = int(raw_input())
-for i in range(t):
-	n , k = map(int , raw_input().split())
-	arr = map(int , raw_input().split())
-	for i in range(n):
-		count = 0
-		n = arr[i]
-		while n:
-			n = n & (n-1)
-			count += 1
-		arr[i] =  count
-	arr = sorted(arr , reverse = True)
- 	print sum(arr[0:k])
+T=int(raw_input())
+while T>0:
+    a1=list(raw_input().split())
+    a2=list(raw_input().split())
+    x=list()
+    for i in a2:
+        x.append(int(bin(int(i)).count('1')))
+    x.sort()
+    c=0
+    i=int(a1[0])-1
+    s=int(a1[1])
+    while s>0:
+        c=c+x[i]
+        i=i-1
+        s=s-1
+    print c
+    T=T-1
